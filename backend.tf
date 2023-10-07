6ba0138fb0ded8302698e059fc29c20291313aca
Fouty7 <amin4712@gmail.com>
2023-10-07 19:39:31 +0100

    Adds ec2 web server with apache installed

--- backend.tf
+++ backend.tf
@@ -4,13 +4,13 @@ terraform {
   ## YOU WILL UNCOMMENT THIS CODE THEN RERUN TERRAFORM INIT
   ## TO SWITCH FROM LOCAL BACKEND TO REMOTE AWS BACKEND
   #############################################################
-   backend "s3" {
-     bucket         = "devops-directive-tf-state-aminundakun" # REPLACE WITH YOUR BUCKET NAME
-     key            = "03-basics/import-bootstrap/terraform.tfstate"
-     region         = "us-east-2"
-     dynamodb_table = "terraform-state-locking"
-     encrypt        = true
-   }
+  backend "s3" {
+    bucket         = "devops-directive-tf-state-aminundakun" # REPLACE WITH YOUR BUCKET NAME
+    key            = "03-basics/import-bootstrap/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
 
   required_providers {
     aws = {
@@ -37,7 +37,7 @@ resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
-  bucket        = aws_s3_bucket.terraform_state.bucket 
+  bucket = aws_s3_bucket.terraform_state.bucket
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
--- main.tf
+++ main.tf
@@ -36,7 +36,66 @@ resource "aws_route_table" "public-RT" {
   }
 }
 
-resource "aws_route_table_association" "a" {
+resource "aws_route_table_association" "RT-association" {
   subnet_id      = aws_subnet.public-SN.id
   route_table_id = aws_route_table.public-RT.id
+}
+
+#data "aws_key_pair" "ssh_key" {
+#  key_name           = "amzonLinux2Key"
+#  include_public_key = true
+#}
+
+resource "aws_instance" "web_server" {
+  ami             = "ami-0430580de6244e02e"
+  instance_type   = "t2.micro"
+  subnet_id       = aws_subnet.public-SN.id
+  associate_public_ip_address = true
+  key_name        = "amzonLinux2Key"
+  security_groups = [aws_security_group.web-server-SG.id]
+  user_data       = <<EOF
+                    #!/bin/bash
+                    sudo apt update -y
+                    sudo apt install apache2 -y
+                    systemctl start apache2
+                    systemctl enable apache2
+                    cd /var/www/html
+                    echo "<h1>Company Website</h1>" > index.html
+                  EOF
+
+  tags = {
+    Name = "terraform-web-server"
+  }
+}
+
+resource "aws_security_group" "web-server-SG" {
+  name        = "web-server-SG"
+  description = "Allow inbound traffic to webserver"
+  vpc_id      = aws_vpc.prod-vpc.id
+
+  ingress {
+    description = "http from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "ssh from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-server-SG"
+  }
 }
